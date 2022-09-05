5bbb3c67ecebe8e16cbbe51a2454f1d75e55bc77
curegit <37978051+curegit@users.noreply.github.com>
2022-09-05 14:35:34 +0900

    Add image.py

---
+++ api/image.py
@@ -0,0 +1,8 @@
+import base64
+from io import BytesIO
+
+def to_png_base64(pil_image):
+	io = BytesIO()
+	pil_image.save(io, "PNG")
+	seq = io.getvalue()
+	return base64.b64encode(seq).decode("ascii")

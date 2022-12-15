7c2aa632995159e0047f6daca6d576e534717e9c
curegit <37978051+curegit@users.noreply.github.com>
2022-12-15 10:21:46 +0900

    Typing

--- api/image.py
+++ api/image.py
@@ -1,7 +1,8 @@
 import base64
 from io import BytesIO
+from PIL.Image import Image
 
-def to_png_base64(pil_image):
+def to_png_base64(pil_image: Image) -> str:
 	io = BytesIO()
 	pil_image.save(io, "PNG")
 	seq = io.getvalue()

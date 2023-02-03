902770068a642ff662de4cfae92d7e7986b8ce70
curegit <37978051+curegit@users.noreply.github.com>
2023-02-03 15:23:25 +0900

    Update util

--- api/util.py
+++ api/util.py
@@ -1,7 +1,7 @@
 import inspect
 from pathlib import Path
 
-def file_rel_path(relpath: str):
+def file_rel_path(relpath: str) -> Path:
 	filename = inspect.stack()[1].filename
 	dirpath = Path(filename).resolve().parent
 	return dirpath.joinpath(relpath).resolve()

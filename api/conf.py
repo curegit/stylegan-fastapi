98c74427f67aad0c17c13768e58a4ec65dd685d0
curegit <37978051+curegit@users.noreply.github.com>
2023-02-03 15:18:55 +0900

    Update conf

--- api/conf.py
+++ api/conf.py
@@ -1,25 +1,26 @@
 import tomllib
 from pathlib import Path
 from pydantic import BaseModel
-from api.util import file_rel_path
 
-class ServerSection(BaseModel):
+class ServerConfig(BaseModel):
 	gpu: bool | int = False
+	lossy: bool = False
 
-class ModelSection(BaseModel):
+class ModelConfig(BaseModel):
 	file: str
 	name: str
 	description: str = ""
 	gpu: bool | int | None = None
+	lossy: bool | None = None
 
 class APIConfig(BaseModel):
 	title: str
 	version: str
 	description: str = ""
-	server: ServerSection
-	models: dict[str, ModelSection]
+	server: ServerConfig
+	models: dict[str, ModelConfig]
 
-def load_config(filepath: str | Path = file_rel_path("../config.toml")):
+def load_config(filepath: str | Path) -> APIConfig:
 	with open(filepath, "rb") as f:
 		obj = tomllib.load(f)
 		return APIConfig(**obj)

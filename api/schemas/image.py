271cb03f11607f138d0159003730b24bf57364ba
curegit <37978051+curegit@users.noreply.github.com>
2023-02-13 22:22:28 +0900

    Update base64

--- api/schemas/image.py
+++ api/schemas/image.py
@@ -1,16 +1,19 @@
 from pydantic import BaseModel, Field
-from api.types import base64
+from api.image import png_mime_type
+from api.types import Base64, base64_examples
 
 class Image(BaseModel):
 	model_id: str = Field(min_length=1, example="animal")
 	conditional: bool = Field(example=True)
 	width: int = Field(ge=1, example=256)
 	height: int = Field(ge=1, example=256)
-	data: base64
+	mime_type: str = Field(example=png_mime_type)
+	data: Base64 = Field(example=next(base64_examples))
 
 class SimpleImage(Image):
 	label: str | None = Field(example="Cat")
-	latent: base64
+	latent: Base64 = Field(example=next(base64_examples))
+	style: Base64 = Field(example=next(base64_examples))
 
 class CompoundImage(Image):
-	styles: list[base64]
+	styles: list[Base64] = Field(min_items=1, example=([next(base64_examples)] * 4 + [next(base64_examples)] * 3))
--- api/types.py
+++ api/types.py
@@ -1,5 +1,12 @@
-from typing import TypeAlias
+from typing import TypeAlias, Iterator
+from itertools import cycle
 
-base64: TypeAlias = str
+Base64: TypeAlias = str
 
-base64_example: base64 = ""
+base64_examples: Iterator[Base64] = cycle([
+	"TWVybWFpZCBNZWxvZHkg8J+OtiBQaWNoaSBQaWNoaSBQaXRjaA==",
+	"U2FpbG9yIE1vb24g8J+MmQ==",
+	"8J+QiOKAjeKsm1Rva3lvIE1ldyBNZXc=",
+	"UHJlY3VyZSDwn6m18J+pt/Cfkps=",
+	"QWlrYXRzdSEg8J+pt/CfjJ/wn46k",
+])

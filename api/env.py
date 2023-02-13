c6a5675433fc300231109518f933746c2ca860ec
curegit <37978051+curegit@users.noreply.github.com>
2023-02-13 16:59:35 +0900

    Update env.py

--- api/env.py
+++ api/env.py
@@ -1,5 +1,6 @@
 import os
+from pathlib import Path
 from api import util
 
 # Configuration file location
-toml_path = os.getenv("STYLEGAN_TOML", default=util.file_rel_path("../config.toml"))
+toml_path: Path = util.resolve_path(os.getenv("STYLEGAN_TOML", default=util.file_rel_path("../config.toml")))

a30cbe64c85553c57e1a73fc6e18e59ec820ad7d
curegit <37978051+curegit@users.noreply.github.com>
2023-02-13 17:04:36 +0900

    Update image.py

--- api/image.py
+++ api/image.py
@@ -1,11 +1,22 @@
 import base64
 from io import BytesIO
 from PIL.Image import Image
-from api.types import base64
+from api.types import Base64
 
-def to_png_base64(pil_image: Image) -> base64:
+png_mime_type: str = "image/png"
+
+def to_png_base64(pil_image: Image) -> Base64:
 	io = BytesIO()
 	pil_image.save(io, "PNG")
+	seq = io.getvalue()
+	string = base64.b64encode(seq).decode("ascii")
+	return string
+
+jpeg_mime_type: str = "image/jpeg"
+
+def to_jpeg_base64(pil_image: Image) -> Base64:
+	io = BytesIO()
+	pil_image.save(io, "JPEG")
 	seq = io.getvalue()
 	string = base64.b64encode(seq).decode("ascii")
 	return string

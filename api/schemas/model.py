369726e30dc16a226e4e26b5b42ea6d5e5ef1c4a
curegit <37978051+curegit@users.noreply.github.com>
2023-02-21 15:52:46 +0900

    Update schemas

--- api/schemas/model.py
+++ api/schemas/model.py
@@ -5,6 +5,6 @@ class Model(BaseModel):
 	name: str = Field(example="My Animal Model")
 	description: str
 	conditional: bool = Field(example=True)
-	labels: list[str] | None = Field(min_items=1, unique_items=True, example=["Cat", "Dog", "Giraffe"])
+	labels: list[str] | None = Field(min_items=1, unique_items=True, example=["Cat", "Dog", "Unicorn"])
 	width: int = Field(ge=1, example=256)
 	height: int = Field(ge=1, example=256)

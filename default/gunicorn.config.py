c31023dfc3439888e9129d2bea557fc061599ae0
curegit <37978051+curegit@users.noreply.github.com>
2023-03-01 21:39:17 +0900

    Fix conf

--- default/gunicorn.config.py
+++ default/gunicorn.config.py
@@ -2,15 +2,13 @@ import os
 import pathlib
 import uvicorn.workers
 
-a = pathlib.Path(__file__).resolve().parent()
-
 wsgi_app = "main:app"
 
 bind = "127.0.0.1:8080"
 daemon = False
 preload_app = True
 workers = os.cpu_count() or 2
-worker_class = uvicorn.workers.UvicornWorker
+worker_class = "uvicorn.workers.UvicornWorker"
 max_requests = 1024
 max_requests_jitter = 128
 graceful_timeout = 60

942396c48b91026906118916aa872ffb2cac31a8
curegit <37978051+curegit@users.noreply.github.com>
2023-03-02 14:47:09 +0900

    Update server exceptions

--- api/exceptions/server.py
+++ api/exceptions/server.py
@@ -1,17 +1,17 @@
 from api.exceptions import HTTPException
 from api.schemas.errors import HTTPError
 
-
-class ProxyException(HTTPException):
+class BadGatewayException(HTTPException):
 
 	status_code = 502
 
-	def __init__(self, msg: str = "") -> None:
+	def __init__(self, msg: str | None = None) -> None:
 		super().__init__(HTTPError(detail=msg))
+
 
 class OverloadedException(HTTPException):
 
 	status_code = 503
 
 	def __init__(self) -> None:
-		super().__init__(HTTPError(detail=""))
+		super().__init__(HTTPError(detail="the server is too busy and temporarily unavailable"))

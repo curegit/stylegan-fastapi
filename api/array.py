a94c018c26a3608e58f1c37fba7f2eb6b920aeb4
curegit <37978051+curegit@users.noreply.github.com>
2023-03-18 00:00:21 +0900

    Update array module

--- api/array.py
+++ api/array.py
@@ -1,20 +1,39 @@
 import base64
 import numpy as np
 from io import BytesIO
+from numpy import ndarray
 from api.types import Base64
-from api.exceptions.client import ArrayValidationException
 
-def to_npy_base64(array: np.ndarray) -> Base64:
+def to_npy_base64(array: ndarray) -> Base64:
 	io = BytesIO()
 	np.save(io, array, allow_pickle=False)
 	seq = io.getvalue()
 	string = base64.b64encode(seq).decode("ascii")
 	return string
 
-def from_npy_base64(npy: Base64) -> np.ndarray:
+def from_npy_base64(npy: Base64) -> ndarray:
 	bs = base64.b64decode(npy)
 	obj = np.load(bs, allow_pickle=False)
-	return obj
+	match obj:
+		case ndarray() as array:
+			return array
+	raise RuntimeError()
 
-def validate_array(array: np.ndarray) -> bool:
+def validate_array(array: ndarray, shape: tuple[int] | tuple[()] | None = None, dtype: str | type | None = None) -> bool:
+	if shape is not None:
+		if array.shape != shape:
+			return False
+	if dtype is not None:
+		if array.dtype != dtype:
+			return False
 	return True
+
+def clamp_array(array: ndarray, min: int | float | None, max: int | float | None, replace_nan: bool | int | float = True):
+	match replace_nan:
+		case False:
+			x = array
+		case True:
+			x = np.nan_to_num(array, posinf=np.inf, neginf=-np.inf)
+		case int() | float() as val:
+			x = np.nan_to_num(array, nan=val, posinf=np.inf, neginf=-np.inf)
+	return np.clip(x, min, max)

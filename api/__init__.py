2d1a5f45defea05ab3551767344485680ebdbc62
curegit <37978051+curegit@users.noreply.github.com>
2023-03-23 10:49:35 +0900

    Fix path resolving

--- api/__init__.py
+++ api/__init__.py
@@ -24,9 +24,9 @@ del utilities
 from api import model
 models: dict[str, model.GeneratorModel] = {
 	key: model.GeneratorModel.load(
-		filepath=(
-			env.toml_path.parent if val.relative else utils.resolve_path(".")
-		).joinpath(val.file).resolve(),
+		filepath=utils.resolve_path(
+			env.toml_path.parent.joinpath(val.file) if val.relative else val.file
+		),
 		id=key,
 		name=val.name,
 		description=val.description,

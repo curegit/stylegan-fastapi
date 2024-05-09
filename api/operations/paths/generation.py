a754c8f9a6ee377858bb0b24a21ac7cdfd66debc
curegit <37978051+curegit@users.noreply.github.com>
2024-05-10 01:39:57 +0900

    Impl all ops

--- api/operations/paths/generation.py
+++ api/operations/paths/generation.py
@@ -1,61 +1,38 @@
 from asyncio import to_thread
+from numpy import ndarray
 from fastapi import APIRouter, Depends
 from api.schemas import SimpleImage
-from api.exceptions import raises_from, responses
-from api.exceptions.client import NotFoundException, ModelNotFoundException, ArrayValidationException
 from api.limit import SpeedLimit
 from api.model import GeneratorModel
-from api.operations.parameters import model, label, psi, latent, sd
+from api.exceptions import raises_from, responses
+from api.operations.parameters import model, optional_label, psi, optional_latent, sd
 from api.operations.dependencies import limit
 
 router = APIRouter(tags=["generation"], dependencies=[Depends(limit)], responses=responses(*raises_from(limit)))
 
-@router.post("/{model_id}/generate", operation_id="generate", response_model=SimpleImage, responses=responses(*raises_from(model, label)))
+@router.post("/{model_id}/generate", operation_id="generate", response_model=SimpleImage, responses=responses(*raises_from(model, optional_label, optional_latent)))
 async def generate(
-		model: GeneratorModel = Depends(model),
-		label: str | None = Depends(label),
-		psi: float = Depends(psi),
-		sd=Depends(sd),
+	model: GeneratorModel = Depends(model),
+	label: tuple[str, int] | None = Depends(optional_label),
+	psi: float = Depends(psi),
+	latent: ndarray | None = Depends(optional_latent),
+	sd: float = Depends(sd),
 ):
 	async with SpeedLimit():
-		latent, style, image, label = await to_thread(
+		latent_str, style, image, label_str = await to_thread(
 			model.generate_encoded,
+			category=(None if label is None else label[1]),
 			psi=psi,
+			mean=latent,
+			sd=sd,
 		)
 	return SimpleImage(
 		model_id=model.id,
-		conditional=model.conditional,
 		width=model.width,
 		height=model.height,
 		mime_type=model.image_type,
 		data=image,
-		label=label,
-		latent=latent,
-		style=style,
-	)
-
-
-@router.post("/{model_id}/regenerate", response_model=SimpleImage, responses=responses(*raises_from(model, label, latent)))
-async def regenerate(
-		model: GeneratorModel = Depends(model),
-		label: str | None = Depends(label),
-		psi: float = Depends(psi),
-		latent=Depends(latent),
-		sd=Depends(sd),
-):
-	async with SpeedLimit():
-		latent, style, image, label = await to_thread(
-			model.generate_encoded,
-			psi=psi,
-		)
-	return SimpleImage(
-		model_id=model.id,
-		conditional=model.conditional,
-		width=model.width,
-		height=model.height,
-		mime_type=model.image_type,
-		data=image,
-		label=label,
-		latent=latent,
+		label=label_str,
+		latent=latent_str,
 		style=style,
 	)
--- api/operations/paths/manipulation.py
+++ api/operations/paths/manipulation.py
@@ -1,23 +1,43 @@
 from asyncio import to_thread
+from numpy import ndarray
 from fastapi import APIRouter, Depends
-from api.schemas.image import SimpleImage, CompoundImage
-from api.exceptions.server import BadGatewayException
-from api.exceptions.server import OverloadedException
+from api.schemas import SimpleImage_, CompoundImage
 from api.limit import SpeedLimit
 from api.model import GeneratorModel
 from api.exceptions import raises_from, responses
-from api.operations.parameters import model
+from api.operations.parameters import model, styles
 from api.operations.dependencies import limit
 
 router = APIRouter(tags=["manipulation"], dependencies=[Depends(limit)], responses=responses(*raises_from(limit)))
 
-@router.post("/{model_id}/blend", response_model=SimpleImage, responses=responses(*raises_from(model)))
-async def blend(model: GeneratorModel = Depends(model)):
+@router.post("/{model_id}/blend", response_model=SimpleImage_, responses=responses(*raises_from(model, styles)))
+async def blend(model: GeneratorModel = Depends(model), styles: list[ndarray] = Depends(styles)):
 	async with SpeedLimit():
-		raise BadGatewayException()
+		style_str, image = await to_thread(
+			model.combine_styles_encoded,
+			styles,
+		)
+	return SimpleImage_(
+		model_id=model.id,
+		width=model.width,
+		height=model.height,
+		mime_type=model.image_type,
+		data=image,
+		style=style_str
+	)
 
-@router.post("/{model_id}/mix", operation_id="mix", response_model=CompoundImage, responses=responses(*raises_from(model)))
-async def mix(model: GeneratorModel = Depends(model)):
+@router.post("/{model_id}/mix", operation_id="mix", response_model=CompoundImage, responses=responses(*raises_from(model, styles)))
+async def mix(model: GeneratorModel = Depends(model), styles: list[ndarray] = Depends(styles)):
 	async with SpeedLimit():
-		styles, image, label = await to_thread()
-	return CompoundImage()
+		styles_str, image = await to_thread(
+			model.mix_styles_encoded,
+			styles,
+		)
+	return CompoundImage(
+		model_id=model.id,
+		width=model.width,
+		height=model.height,
+		mime_type=model.image_type,
+		data=image,
+		styles=styles_str,
+	)

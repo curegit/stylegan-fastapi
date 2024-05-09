901ec0af94dc7897043e3a12e6c11260d5a9920c
curegit <37978051+curegit@users.noreply.github.com>
2024-05-10 01:48:12 +0900

    Fix err schemas

--- api/schemas/errors.py
+++ api/schemas/errors.py
@@ -4,16 +4,24 @@ from pydantic import BaseModel
 class HTTPError(BaseModel):
 	detail: Any = None
 
+
 class NotFoundError(BaseModel):
 	type: str
 	name: str
 	msg: str
 
+
 class HTTPNotFoundError(HTTPError):
 	detail: NotFoundError | None = None
 
+
 class DeserializationError(HTTPError):
 	detail: str | None = None
+
+
+class EntityValidationError(HTTPError):
+	pass
+
 
 class RequestLimitError(HTTPError):
 	detail: str | None = None
